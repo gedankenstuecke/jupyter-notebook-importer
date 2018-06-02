5889b2b2d71efa0c961dcbd2c1ef8cdb2f0d6168
Bastian Greshake Tzovaras <bgreshake@googlemail.com>
2018-06-02 13:44:01 -0700

    better formatting file name

--- oh_notebook_importer/helper.py
+++ oh_notebook_importer/helper.py
@@ -7,9 +7,9 @@ def import_notebook(notebook_url, notebook_name):
     notebook_content = requests.get(notebook_url).content
     if os.path.isfile(notebook_name):
         notebook_name = notebook_name.replace('.ipynb',
-                                              '{}.ipynb'.format(
+                                              '-{}.ipynb'.format(
                                                 arrow.now().format(
-                                                'YYYY-MM-DD-HH:mm:ss' 
+                                                'YYYY-MM-DD-HH:mm:ss'
                                                 )))
     with open(notebook_name,'wb') as f:
         f.write(notebook_content)
